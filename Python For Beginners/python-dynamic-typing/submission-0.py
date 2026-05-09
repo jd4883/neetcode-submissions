5034395ffd7aa2958ade7b51368a7ccb678392b5
Condoriano <15719920+jd4883@users.noreply.github.com>
2026-05-08 17:12:49 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "this"
+print(type(variable))
+variable = []
+print(type(variable))
