a95b12606b17f759e88030c00508f532779cd63b
Condoriano <15719920+jd4883@users.noreply.github.com>
2026-05-18 10:33:58 -0700

    Add: last-stone-weight - submission-7

---
+++ Data Structures & Algorithms/last-stone-weight/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        rocks = stones.copy()
+        rocks.sort()
+        while rocks:
+            if len(rocks) == 1:
+                return sum(rocks)
+            y = rocks.pop()
+            x = rocks.pop()
+            if x < y:
+                rocks.append(abs(y-x))
+                rocks.sort()
+        return 0
